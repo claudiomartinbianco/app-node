5a39c7c993989e34a255e0766ae38e56cee2e0a7
Claudio Bianco <claudiombianco@hotmail.com>
2021-11-26 17:12:25 -0300

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-var port = process.env.port || 3000;
-
-app.listen(port, () => console.log(`Server running on localhost:${port}`));
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
