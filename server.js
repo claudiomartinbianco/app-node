efdede8602e568050a3822519823b1f6bfddd3d8
Claudio Bianco <claudiombianco@hotmail.com>
2021-11-26 16:43:41 -0300

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.port || "1337";
+app.set("port", port);
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
-var port = process.env.port || 80;
 
 app.listen(port)
 console.log(`Listening on ${ PORT }`)
